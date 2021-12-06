c5593be9e4af2c1bd65abe6911d114dfb315d8ff
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-12-06 01:19:16 -0500

    added multiple servos

--- SpiderController.py
+++ SpiderController.py
@@ -35,20 +35,29 @@ innerElbowRight = 9
 # INNER WRISTS
 innerWristLeft = 10
 innerWristRight = 11
-angle = 90
-
+angle0 = 90
+angle1 = 90
+angle2 = 90
 try:
-    kit.servo[outerShoulderLeft].angle = angle
-    print(str(angle))
+    kit.servo[outerShoulderLeft].angle = angle0
     while True:
         char = screen.getch()
         if char == ord('q'):
-            if angle < 180:
-                angle += 1
+            if angle0 < 180:
+                angle0 += 1
         if char == ord('a'):
-            if angle > 0:
-                angle -= 1
-        kit.servo[outerShoulderLeft].angle = angle
+            if angle0 > 0:
+                angle0 -= 1
+        if char == ord('s'):
+            if angle1 < 180:
+                angle1 += 1
+        if char == ord('w'):
+            if angle0 > 0:
+                angle1 -= 1
+        kit.servo[outerShoulderLeft].angle = angle0
+        kit.servo[outerShoulderRight].angle = angle0
+        kit.servo[outerElbowLeft].angle = angle1
+        kit.servo[outerElboRight].angle = angle1
 except KeyboardInterrupt:
     Servos = 0
     while Servos < 16:
