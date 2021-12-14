b1d45c6f1b84489e7e11339d5bd8609af3deb77c
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-12-14 01:33:37 -0500

    pic

--- pic.py
+++ pic.py
@@ -1,8 +1,8 @@
-from picamera import PiCamera
+from pic import PiCamera
 import pigpio
 from datetime import datetime
 camera = PiCamera()
 def capture():
-    timestamp = datetime.no().isoformat()
+    timestamp = datetime.now().isoformat()
     camera.capture('/home/pi%s.jpg' % timestamp)
 capture()
